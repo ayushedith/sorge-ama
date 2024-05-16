ac6cbf703da1306afe2b03da0b5933a06fe501ad
Ayush <ayushedith@gmail.com>
2024-05-17 01:29:36 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import AuthProvider from '../context/AuthProvider';
+import { Toaster } from '@/components/ui/toaster';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'IncogChat',
+  description: 'Anonymously connect and chat securely, without revealing your identity.',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" >
+      <AuthProvider>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
+      </AuthProvider>
+    </html>
+  );
+}
+
